9b73e06e8065371375f8b7b242bf7ce9553107f2
mx5566 <2473321894@qq.com>
2020-04-05 15:48:05 +0800

    Create main.go

---
+++ example/main.go
@@ -0,0 +1,19 @@
+// Copyright 2020 Andy Pan. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+/**
+ * @Author: meng
+ * @Description:
+ * @File:  main.go
+ * @Version: 1.0.0
+ * @Date: 2020/4/5 15:46
+ */
+
+package example
+
+import "fmt"
+
+func main() {
+	fmt.Print("HelloWorld")
+}
